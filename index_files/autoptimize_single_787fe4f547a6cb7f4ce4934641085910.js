261bfed7b02ba714731fc606e82fafa494a25ea1
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2023-11-15 16:30:12 +1000

    restore_archive

--- index_files/autoptimize_single_787fe4f547a6cb7f4ce4934641085910.js
+++ index_files/autoptimize_single_787fe4f547a6cb7f4ce4934641085910.js
@@ -1,2 +1,37 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
 (function($){'use strict';$(document).ready(function(){$('body').on('adding_to_cart',function(event,$button,data){if($button&&$button.hasClass('vc_gitem-link')){$button.addClass('vc-gitem-add-to-cart-loading-btn').parents('.vc_grid-item-mini').addClass('vc-woocommerce-add-to-cart-loading').append($('<div class="vc_wc-load-add-to-loader-wrapper"><div class="vc_wc-load-add-to-loader"></div></div>'));}}).on('added_to_cart',function(event,fragments,cart_hash,$button){if('undefined'===typeof($button)){$button=$('.vc-gitem-add-to-cart-loading-btn');}
 if($button&&$button.hasClass('vc_gitem-link')){$button.removeClass('vc-gitem-add-to-cart-loading-btn').parents('.vc_grid-item-mini').removeClass('vc-woocommerce-add-to-cart-loading').find('.vc_wc-load-add-to-loader-wrapper').remove();}});});})(window.jQuery);
+
+}
+/*
+     FILE ARCHIVED ON 15:19:58 Aug 26, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 05:34:54 Nov 15, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 125.856
+  exclusion.robots: 0.069
+  exclusion.robots.policy: 0.06
+  cdx.remote: 0.053
+  esindex: 0.008
+  LoadShardBlock: 41.975 (3)
+  PetaboxLoader3.datanode: 74.991 (5)
+  load_resource: 73.141
+  PetaboxLoader3.resolve: 32.5
+  loaddict: 25.147
+*/
